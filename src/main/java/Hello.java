57f4ad6af6508353aa5213fca44734087b732686
mark.zhang <mark.zhang2@homecredit.cn>
2020-05-26 13:52:34 +0800

    Add photo generate.

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String args[]){
+        String photoPath = System.getProperty("user.dir")+"\\src\\main\\resources\\photo\\background.jpg";
+        String targetPhoto = System.getProperty("user.dir")+"\\src\\main\\resources\\photo\\new.jpg";
+        CommonUtil commonUtil=new CommonUtil();
+        commonUtil.generateIdCard(photoPath,targetPhoto,"120222194854348521","张三");
         System.out.println("Hello");
+
+
     }
 }
