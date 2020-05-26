6a16fd286e1b2fa12899c8900eefff9f395ddfdb
mark.zhang <mark.zhang2@homecredit.cn>
2020-05-27 06:46:00 +0800

    Update jenkins file

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,11 @@
 public class Hello {
     public static void main(String args[]){
-        String photoPath = System.getProperty("user.dir")+"\\src\\main\\resources\\photo\\background.jpg";
-        String targetPhoto = System.getProperty("user.dir")+"\\src\\main\\resources\\photo\\new.jpg";
+//        String photoPath = System.getProperty("user.dir")+"\\src\\main\\resources\\photo\\background.jpg";
+//        String targetPhoto = System.getProperty("user.dir")+"\\src\\main\\resources\\photo\\new.jpg";
+        String photoPath = System.getProperty("user.dir")+"\\src\\main\\java\\photo\\background.jpg";
+        String targetPhoto = System.getProperty("user.dir")+"\\src\\main\\java\\photo\\new.jpg";
+        System.out.println("photoPath "+photoPath);
+        System.out.println("targetPhoto "+targetPhoto);
         CommonUtil commonUtil=new CommonUtil();
         commonUtil.generateIdCard(photoPath,targetPhoto,"120222194854348521","张三");
         System.out.println("Hello");
